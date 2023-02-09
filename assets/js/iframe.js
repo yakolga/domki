69bc7ef76dce2bdba0783ec163e8b426f42a363e
Your Name <you@example.com>
2023-02-09 10:39:31 +0100

    changes

---
+++ assets/js/iframe.js
@@ -0,0 +1,23 @@
+lightGallery(document.getElementById('open-website1'), {
+  selector: 'this',
+});
+
+lightGallery(document.getElementById('open-website2'), {
+  selector: 'this',
+});
+
+lightGallery(document.getElementById('open-website3'), {
+  selector: 'this',
+});
+
+lightGallery(document.getElementById('open-website4'), {
+  selector: 'this',
+});
+
+lightGallery(document.getElementById('open-website5'), {
+  selector: 'this',
+});
+
+lightGallery(document.getElementById('open-website6'), {
+  selector: 'this',
+});
